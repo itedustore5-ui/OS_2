2d1266a3cd974c255f068cf07dfa062b95337313
itedustore <itedustore5@gmail.com>
2026-04-21 12:16:20 +0200

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -37,7 +37,7 @@ app.use("/api", router);
 // Servira frontend
 const frontendDist = path.resolve(__dirname, "../../../srpski-kviz/dist/public");
 app.use(express.static(frontendDist));
-app.get("*", (_req, res) => {
+app.get("/{*path}", (_req, res) => {
   res.sendFile(path.join(frontendDist, "index.html"));
 });
 
